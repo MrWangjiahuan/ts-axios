7bbdbe28d4a67fac467d28ac1188a81a06e6b67f
wangjiahuan <wangjiah>
2019-04-30 14:21:35 +0800

    feat(helpers=>headers.ts): create auxiliary function headers.ts
    Processing header content-type

---
+++ src/helpers/headers.ts
@@ -0,0 +1,23 @@
+import { isPlainObject } from './util'
+
+function normalizeHederName(headers: any, normalizedName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).map(name => {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data: any): any {
+  normalizeHederName(headers, 'Content-Type')
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8'
+    }
+  }
+  return headers
+}
