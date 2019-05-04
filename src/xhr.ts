8991194e45aa56bfcae5b43840e199c6f41d70e1
wangjiahuan <wangjiah>
2019-05-04 10:44:01 +0800

    feat(all the file): add promise
    Add to request header object parsing and add promise chain calls

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,10 +1,44 @@
-import { AxiosRequestConfig } from './types'
-export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
-  const request = new XMLHttpRequest()
-  // method默认要求大写
-  // async 默认异步
-  request.open(method.toUpperCase(), url, true)
-  request.send(data)
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
+
+    const request = new XMLHttpRequest()
+    if (responseType) {
+      request.responseType = responseType
+    }
+
+    // method默认要求大写
+    // async 默认异步
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    Object.keys(headers).map(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+  })
 }
